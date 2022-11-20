a15830601ad6c21710043700740a320c04fa634f
Алексей Кузьменко <wowalexey@gmail.com>
2022-11-20 21:43:29 +0300

    все исправления, кроме того что у Виталия

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,20 +1,24 @@
-import datetime
+from enum import Enum
 
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import (MaxValueValidator, MinValueValidator,
                                     RegexValidator)
 from django.db import models
 
-ROLE_CHOICES = [('user', 'пользователь'),
-                ('moderator', 'модератор'),
-                ('admin', 'администратор')]
+from reviews.validators import validate_year
+
+
+class UserRole(Enum):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    @staticmethod
+    def get_all_roles():
+        return tuple((i.value, i.name) for i in UserRole)
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-        validators=[RegexValidator(regex=r'^[\w.@+-]+$')])
     email = models.EmailField(
         max_length=254,
         unique=True)
@@ -28,33 +32,33 @@ class CustomUser(AbstractUser):
         blank=True)
     role = models.CharField(
         max_length=20,
-        choices=ROLE_CHOICES,
-        default='user')
+        choices=UserRole.get_all_roles(),
+        default=UserRole.USER.value)
 
     class Meta:
-        ordering = ['username']
+        ordering = ('username',)
 
     def __str__(self):
         return self.username
 
     @property
     def is_admin(self):
-        return self.role == 'admin' or self.is_staff
+        return self.role == UserRole.ADMIN.value or self.is_staff
 
     @property
     def is_moderator(self):
-        return self.role == 'moderator'
+        return self.role == UserRole.MODERATOR.value
 
 
 class Category(models.Model):
     name = models.CharField(
-        'имя категории',
+        'Имя категории',
         max_length=256)
     slug = models.SlugField(
-        'слаг категории',
+        'Слаг категории',
         max_length=50,
         unique=True,
-        validators=[RegexValidator(regex=r'^[-a-zA-Z0-9_]+$')])
+        validators=(RegexValidator(regex=r'^[-a-zA-Z0-9_]+$'),))
 
     class Meta:
         verbose_name = 'Категория'
@@ -66,10 +70,10 @@ class Category(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(
-        'имя жанра',
+        'Имя жанра',
         max_length=200)
     slug = models.SlugField(
-        'cлаг жанра',
+        'Слаг жанра',
         unique=True)
 
     class Meta:
@@ -82,28 +86,27 @@ class Genre(models.Model):
 
 class Title(models.Model):
     name = models.CharField(
-        'название',
+        'Название',
         max_length=200)
     year = models.IntegerField(
-        'год',
-        validators=[MaxValueValidator(datetime.date.today().year,
-                    'Неверно введен год')])
+        'Год',
+        validators=(validate_year,))
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         related_name='titles',
-        verbose_name='категория',
+        verbose_name='Категория',
         null=True,
         blank=True)
     description = models.TextField(
-        'описание',
+        'Описание',
         max_length=255,
         null=True,
         blank=True)
     genre = models.ManyToManyField(
         Genre,
         related_name='titles',
-        verbose_name='жанр')
+        verbose_name='Жанр')
 
     class Meta:
         verbose_name = 'Произведение'
@@ -136,14 +139,15 @@ class Review(models.Model):
         Title,
         on_delete=models.CASCADE,
         related_name='reviews')
-    text = models.TextField('текст отзыва')
+    text = models.TextField('Текст отзыва')
     author = models.ForeignKey(
         CustomUser,
         on_delete=models.CASCADE,
         related_name='reviews')
-    score = models.IntegerField(
-        validators=[MinValueValidator(0),
-                    MaxValueValidator(10)])
+    score = models.PositiveSmallIntegerField(
+        validators=(MinValueValidator(0, 'Оценка не может быть меньше 0'),
+                    MaxValueValidator(10, 'Оценка происходит '
+                                          'по 10-ти бальной шкале')))
     pub_date = models.DateTimeField(
         'Дата добавления',
         auto_now_add=True)
@@ -166,14 +170,14 @@ class Comment(models.Model):
     review = models.ForeignKey(
         Review, on_delete=models.CASCADE, related_name='comments'
     )
-    text = models.TextField('текст комментария')
+    text = models.TextField('Текст комментария')
     author = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, related_name='comments')
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
 
     class Meta:
-        ordering = ['-pub_date']
+        ordering = ('-pub_date',)
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
 
